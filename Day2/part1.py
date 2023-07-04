5f2158997f61242e7eb3396d5af1aff7e3dc9810
abdullah-imtiaz-bboxx <a.imtiaz@bboxx.com>
2023-07-04 16:49:29 +0500

    Remove print statement, update dictionary keys

--- Day2/part1.py
+++ Day2/part1.py
@@ -9,9 +9,9 @@ my_moves = {"X": Rock, "Y": Paper, "Z": Scissors}
 score = {Rock: 1, Paper: 2, Scissors: 3}
 outcome = {"lost": 0, "draw": 3, "won": 6}
 # Rock defeats Scissors etc
-defeats = {"Rock": "Scissors", "Scissors": "Paper", "Paper": "Rock"}
+defeats = {Rock: Scissors, Scissors: Paper, Paper: Rock}
 
-file_name = 'sample_input2.txt'
+file_name = 'sample_input.txt'
 
 
 def file_read_gen(file):
@@ -32,7 +32,6 @@ def compute_outcome_of_game(file_name):
                 score_of_round.append(outcome['won'] + score[my_move])
             # Draw
             elif opponenets_move == my_move:
-                print(round, opponenets_move, my_move, defeats[my_move])
                 score_of_round.append(outcome['draw'] + score[my_move])
             # Lost
             else:
