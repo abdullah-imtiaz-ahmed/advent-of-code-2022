5c963c20c8b2958dfacecd0f0d8c140bff064c93
abdullah-imtiaz-bboxx <a.imtiaz@bboxx.com>
2023-07-05 17:33:30 +0500

    Day 4, Part 2

---
+++ Day4/part2.py
@@ -0,0 +1,28 @@
+file_name = "sample_input.txt"
+
+
+def file_read_gen(file):
+    for line in file:
+        line = line.strip().split(",")
+        yield line
+
+
+def camp_cleanup(file_name):
+    pairs_that_overlap = 0
+    with open(file_name, "r") as file:
+        for item in file_read_gen(file):
+            first, second = item
+            first, second = generate_sections(first), generate_sections(second)
+            if not first.isdisjoint(second):
+                pairs_that_overlap += 1
+
+    return pairs_that_overlap
+
+
+def generate_sections(range_):
+    start, end = [int(value) for value in range_.split("-")]
+    return set(range(start, end + 1))
+
+
+if __name__ == "__main__":
+    print(camp_cleanup(file_name))
