b2e5dab6e0610b5117bd6809473cd09acdfa6681
abdullah-imtiaz-bboxx <a.imtiaz@bboxx.com>
2023-07-06 15:59:30 +0500

    Day 5, Part 1

---
+++ Day5/part1.py
@@ -0,0 +1,62 @@
+import re
+import itertools
+
+file_name = "sample_input.txt"
+
+
+def read_and_parse_file(file):
+    stacks, moves = [], []
+    parse_moves = False
+    for line in file:
+        if line.strip() == "":
+            parse_moves = True
+            continue
+
+        elif not parse_moves:
+            line = list(line)
+            crates = []
+            for index in range(0, len(line), 4):
+                index_slice = slice(index, index + 4)
+                line_data = line[index_slice]
+                crates.append("".join(line_data).strip())
+            stacks.append(crates)
+
+        else:
+            move = [int(digit) for digit in re.findall(r"\d+", line)]
+            moves.append(move)
+    # print(stacks)
+    return stacks, moves
+
+
+def supply_stacks(file_name):
+    with open(file_name, "r") as file:
+        stacks, moves = read_and_parse_file(file)
+        # print(filter_empty_values(stacks))
+        stacks = filter_empty_values(matrix_transpose(stacks[:-1]))
+        # print(stacks)
+    for move in moves:
+        number_of_items, from_, to = move
+        from_, to = from_ - 1, to - 1
+        for _ in range(number_of_items):
+            value = stacks[from_].pop(0)
+            stacks[to].insert(0, value)
+
+    crate_on_top_of_stack = []
+    for stack in stacks:
+        crate_on_top_of_stack.append(stack.pop(0)[1])
+    return "".join(crate_on_top_of_stack)
+
+
+def matrix_transpose(matrix):
+    return [list(item) for item in itertools.zip_longest(*matrix)]
+
+
+def filter_empty_values(stacks):
+    filtered_stack = []
+    for stack in stacks:
+        filtered_stack.append([value for value in stack if value])
+    return filtered_stack
+
+
+if __name__ == "__main__":
+    print(supply_stacks(file_name))
