794c8914cf6db33a3c65e53c56e815848d064862
abdullah-imtiaz-bboxx <a.imtiaz@bboxx.com>
2023-07-07 13:37:15 +0500

    Update marker

--- Day6/part1.py
+++ Day6/part1.py
@@ -4,10 +4,10 @@ file_name = "test_input.txt"
 def tuning_trouble(file_name):
     with open(file_name, "r") as f:
         data = f.read().replace("\n", "")
-
-    for index in range(0, len(data) - 4):
-        if len(set(data[index:index + 4])) == 4:
-            return index + 4
+    marker = 4
+    for index in range(0, len(data) - marker):
+        if len(set(data[index:index + marker])) == marker:
+            return index + marker
     return 0
 
 
